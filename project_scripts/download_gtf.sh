e403542ab54fdb31347e444f47c8c933fe4664e8
Caden McQuillen <54229205+cadenmcquillen@users.noreply.github.com>
2023-04-11 17:17:14 -0400

    Update download_gtf.sh

--- project_scripts/download_gtf.sh
+++ project_scripts/download_gtf.sh
@@ -8,6 +8,6 @@
 #SBATCH --mem=8G
 
 cd /athena/angsd/scratch/cnm4001/hg38
-wget 'https://www.gencodegenes.org/human/' -O hg38.gencodev43.gtf.gz
+wget 'https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_43/gencode.v43.annotation.gtf.gz' -O hg38.gencodev43.gtf.gz
 
 
