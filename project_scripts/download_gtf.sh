f462077c3dfcad9a93a1b6de2eb39cf524e8beb7
Caden McQuillen <caden.mcquillen@gmail.com>
2023-04-11 14:33:20 -0400

    added some rerun changes

---
+++ project_scripts/download_gtf.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -l
+
+#SBATCH --partition=angsd_class
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=download
+#SBATCH --time=02:00:00 # HH/MM/SS
+#SBATCH --mem=8G
+
+cd /athena/angsd/scratch/cnm4001/hg38
+wget 'https://www.gencodegenes.org/human/' -O gencodev43.gtf.gz
+
+
---
+++ project_scripts/reRun_aligmentQC.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -l
+
+#SBATCH --partition=angsd_class
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=align_qc
+#SBATCH --time=03:00:00 # HH/MM/SS
+#SBATCH --mem=20G
+
+
+cd /athena/angsd/scratch/cnm4001/NPC_alignmentQC
+for index in 597 598 599 600 601 602; do
+  mkdir reRun_SRR11849${index}_alignmentQC
+done
+
+cd /athena/angsd/scratch/cnm4001/NPC_bams
+mamba activate qorts
+
+for index in 597 598 599 600 601 602; do
+  qorts -Xmx18000M QC \
+  --generatePlots \
+  --singleEnded \
+   SRR11849${index}.Aligned.sortedByCoord.out.bam \
+  /athena/angsd/scratch/cnm4001/hg38/hg38.gencodev43.gtf  \
+  /athena/angsd/scratch/cnm4001/NPC_alignmentQC/reRun_SRR11849${index}_alignmentQC
+done
+
+
